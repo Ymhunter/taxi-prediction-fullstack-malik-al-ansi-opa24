7969f0436ca722f673c84fcf9362d2dab4e14cf0
Malik <imalikril@gmail.com>
2025-09-17 18:46:58 +0200

    prep: adding Model path

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -1,5 +1,7 @@
 from importlib.resources import files
-
+import os
 TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
 
 # DATA_PATH = Path(__file__).parents[1] / "data"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # src/taxipred
+MODEL_PATH = os.path.join(BASE_DIR, "data", "random_forest_model.sav")
